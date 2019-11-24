1b081d0687044cac69572f67ab4e4a416c2d2c98
A8C304FB91E0FC3D2AD5F3C4F59BAA2F <A8C304FB91E0FC3D2AD5F3C4F59BAA2F@i-search.com.cn>
2019-11-24 22:51:05 +0800

    feature: user json 数据检验

---
+++ src/validator/_validate.js
@@ -0,0 +1,23 @@
+/**
+ * @description json schema 校验
+ * @author lqz
+ */
+
+const AJV = require('ajv');
+const ajv = new AJV({
+    // allErrors : true // 输出所有错误(比较慢)
+});
+
+/**
+ * json schema 校验
+ * @param schema
+ * @param data
+ */
+function validate(schema, data = {}) {
+    const valid = ajv.validate(schema, data);
+    if (!valid) {
+        return valid.errors[0];
+    }
+}
+
+module.exports = validate;
